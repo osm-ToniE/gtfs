128448558cdb79e3a2ed120087f878c804c40c8a
Toni Erdmann <Toni.Erdmann@web.de>
2020-04-20 15:52:06 +0200

    remove --dry-run from rsync

--- bin/gtfs-handle-zip.sh
+++ bin/gtfs-handle-zip.sh
@@ -30,9 +30,19 @@ if [ "$D1" = "gtfs-networks" ]
 then
     TARGET_DB="$WORK_BASE_DIR/$D2/$D2-$D3-$DB"
     echo $TARGET_DB
+    
+    if [ "$D2" = "DE" ]
+    then
+        ANALYSIS_LANG="--language=de"
+    fi
 else
     TARGET_DB="$WORK_BASE_DIR/$D1/$D2/$D1-$D2-$D3-$DB"
     echo $TARGET_DB
+    
+    if [ "$D1" = "DE" ]
+    then
+        ANALYSIS_LANG="--language=de"
+    fi
 fi
 
 
@@ -42,11 +52,11 @@ gtfs-prepare-ptna-sqlite.sh $*
 echo $(date '+%Y-%m-%d %H:%M:%S') "start aggregation"
 gtfs-aggregate-ptna-sqlite.pl $*
 
-echo $(date '+%Y-%m-%d %H:%M:%S') "start analysis"
-gtfs-analyze-ptna-sqlite.pl $*
+echo $(date '+%Y-%m-%d %H:%M:%S') "start analysis $ANALYSIS_LANG"
+gtfs-analyze-ptna-sqlite.pl $ANALYSIS_LANG $*
 
 echo $(date '+%Y-%m-%d %H:%M:%S') "start normalization"
 gtfs-normalize-ptna-sqlite.pl $*
 
 echo $(date '+%Y-%m-%d %H:%M:%S') "start rsync -rtvu $DB $TARGET_DB"
-rsync -rtvu --dry-run $DB $TARGET_DB
+rsync -rtvu $DB $TARGET_DB
