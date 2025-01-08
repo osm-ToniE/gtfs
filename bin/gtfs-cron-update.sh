891fc661ad35b7f42265e04fa108bf4d6d99c17c
Toni Erdmann <Toni.Erdmann@web.de>
2025-01-08 12:36:56 +0100

    gtfs-cron-update.sh must not run on a 'tty'

--- bin/gtfs-cron-update.sh
+++ bin/gtfs-cron-update.sh
@@ -8,33 +8,30 @@ tty_ret=$?
 
 if [ $tty_ret -ne 0 ]
 then
+    echo $(date "+%Y-%m-%d %H:%M:%S %Z") "gtfs-cron-update.sh: start"
+
     if [ -f ~/.ptna-config ]
     then
         source ~/.ptna-config
+
+        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "$(top -bn1 | grep -i '^.CPU')"
+        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "$(df | grep 'osm')"
+        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "gtfs-all-feeds.sh -cDT"
+
+        gtfs-all-feeds.sh -cDT | tee "$PTNA_WORK_LOC/gtfs-all-feeds-cDT.log"
+
+        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "gtfs-update-from-file.sh"
+
+        gtfs-update-from-file.sh -l -f "$PTNA_WORK_LOC/gtfs-all-feeds-cDT.log"
+
+        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "$(top -bn1 | grep -i '^.CPU')"
+        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "$(df | grep 'osm')"
+        echo $(date "+%Y-%m-%d %H:%M:%S %Z") "gtfs-cron-update.sh: done"
     else
         echo $(date "+%Y-%m-%d %H:%M:%S %Z") "~/.ptna-config not found, terminating"
         exit 1
     fi
+else
+    echo $(date "+%Y-%m-%d %H:%M:%S %Z") "Called from a 'tty', terminating"
+    exit 2
 fi
-
-echo "#####################################"
-echo ""
-echo $(date "+%Y-%m-%d %H:%M:%S %Z") "gtfs-all-feeds.sh -cDT"
-echo ""
-echo "#####################################"
-
-gtfs-all-feeds.sh -cDT | tee "$PTNA_WORK_LOC/gtfs-all-feeds-cDT.log"
-
-echo "#####################################"
-echo ""
-echo $(date "+%Y-%m-%d %H:%M:%S %Z") "gtfs-update-from-file.sh"
-echo ""
-echo "#####################################"
-
-gtfs-update-from-file.sh -l -f "$PTNA_WORK_LOC/gtfs-all-feeds-cDT.log"
-
-echo "#####################################"
-echo ""
-echo $(date "+%Y-%m-%d %H:%M:%S %Z") "gtfs-cron-update.sh: done"
-echo ""
-echo "#####################################"
