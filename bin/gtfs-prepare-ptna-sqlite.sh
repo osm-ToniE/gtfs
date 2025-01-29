db08630d6a3b1f0f652fc6bb6797c31f0af744a5
Toni Erdmann <Toni.Erdmann@web.de>
2025-01-29 13:33:12 +0100

    fix error in type and default of columns start_date and end_date of calendar table

--- bin/gtfs-prepare-ptna-sqlite.sh
+++ bin/gtfs-prepare-ptna-sqlite.sh
@@ -244,7 +244,7 @@ then
         exit 1
     fi
 fi
-columns="service_id TEXT PRIMARY KEY, monday INTEGER DEFAULT 0, tuesday INTEGER DEFAULT 0, wednesday INTEGER DEFAULT 0, thursday INTEGER DEFAULT 0, friday INTEGER DEFAULT 0, saturday INTEGER DEFAULT 0, sunday INTEGER DEFAULT 0, start_date TEXT DEFAULT '', end_date TEXT DEFAULT ''"
+columns="service_id TEXT PRIMARY KEY, monday INTEGER DEFAULT 0, tuesday INTEGER DEFAULT 0, wednesday INTEGER DEFAULT 0, thursday INTEGER DEFAULT 0, friday INTEGER DEFAULT 0, saturday INTEGER DEFAULT 0, sunday INTEGER DEFAULT 0, start_date INTEGER DEFAULT 0, end_date INTEGER DEFAULT 0"
 sqlite3 $SQ_OPTIONS "$DB" "CREATE TABLE IF NOT EXISTS calendar ($columns);"
 sqlite3 $SQ_OPTIONS "$DB" "INSERT OR IGNORE INTO calendar (service_id) SELECT DISTINCT service_id FROM calendar_dates;"
 sqlite3 $SQ_OPTIONS "$DB" "UPDATE calendar SET start_date = (SELECT date FROM calendar_dates ORDER BY CAST (date AS INTEGER) ASC  LIMIT 1) WHERE start_date = 0;"
